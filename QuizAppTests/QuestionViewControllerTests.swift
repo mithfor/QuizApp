b9b529a2fc29b91301b4f976e1080cbc6194d131
Dmitriy Voronin <devoronin@gmail.com>
2022-09-15 13:17:58 +0300

    QuestionViewController UnitTests Refactoring

--- QuizAppTests/QuestionViewControllerTests.swift
+++ QuizAppTests/QuestionViewControllerTests.swift
@@ -13,37 +13,35 @@ class QuestionViewControllerTest: XCTestCase {
     
     func test_viewDidLoad_renderQuestionHeaderText() {
         
-        let sut = QuestionViewController(question: "Q1", options: [])
-        
-        _ = sut.view
+        let sut = makeSut(question: "Q1")
         
         XCTAssertEqual(sut.headerLabel.text, "Q1")
     }
     
     func test_viewDidLoad_withNoOptions_rendersZeroOptions() {
-        let sut = QuestionViewController(question: "Q1", options: [])
-        
-        _ = sut.view
+        let sut = makeSut()
         
         XCTAssertEqual(sut.tableView.numberOfRows(inSection: 0), 0)
     }
     
     func test_viewDidLoad_withOneOption_rendersOneOption() {
-        let sut = QuestionViewController(question: "Q1", options: ["A1"])
-        
-        _ = sut.view
+        let sut = makeSut(options: ["A1"])
         
         XCTAssertEqual(sut.tableView.numberOfRows(inSection: .zero), 1)
     }
     
     func test_viewDidLoad_withOneOption_rendersOneOptionText() {
-        let sut = QuestionViewController(question: "Q1", options: ["A1"])
-        
-        _ = sut.view
+        let sut = makeSut(options: ["A1"])
         
         let indexPath = IndexPath(row: 0, section: 0)
         let cell = sut.tableView.dataSource?.tableView(sut.tableView, cellForRowAt: indexPath)
         
         XCTAssertEqual(cell?.textLabel?.text, "A1")
+    }
+    
+    func makeSut(question: String = "", options: [String] = []) -> QuestionViewController {
+        let sut = QuestionViewController(question: question, options: options)
+        _ = sut.view
+        return sut
     }
 }
